a1b2048ce49097d03a4ab3ab17bea1da281f21f0
Ahmed Ebeid <ahmed.ebeid@ibm.com>
2022-10-16 15:13:34 +0200

    init

---
+++ src/com/Ibm/example/MainApp.java
@@ -0,0 +1,14 @@
+package com.Ibm.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+		Sample a = (Sample) context.getBean("sample");
+		a.printAnyThing();
+	}
+
+}
---
+++ src/com/Ibm/example/Sample.java
@@ -0,0 +1,8 @@
+package com.Ibm.example;
+
+public class Sample {
+	void printAnyThing() {
+		System.out.println("Hello Spring");
+	}
+
+}
